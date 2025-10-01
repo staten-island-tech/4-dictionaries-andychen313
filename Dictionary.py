89c93b38111765188357eebf8dc6294122d4decd
andychen313 <andyc313@nycstudents.net>
2025-10-01 09:33:16 -0400

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+market_buy_items = [
+    {
+        "name": "Banana",
+        "price": "0.59"
+        "type of item": "Fruit"
+        "description": "This fruit has a yellow skin and contains potassium."
+    },
+
+    {    
+        "name": "Apple",
+        "price": "1.25"
+        "type of item": "Fruit"
+        "description": "This fruit is round and has a red skin. The fruit is moderately hard."
+    },
+
+    {
+        "name": "Orange",
+        "price": "1.10"
+        "type of item": "Fruit"
+        "description": "This fruit is is round and has an orange skin. The fruit contains citrus."
+
+    }
+
+
+]
