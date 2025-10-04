23a6d4b3e791118be64f384844086599035c4ef1
andychen313 <andyc313@nycstudents.net>
2025-10-04 16:54:20 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,31 +1,59 @@
-""" market_buy_items = [
-    {
-        "name": "Banana",
-        "price": "0.59"
-        "type of item": "Fruit"
-        "description": "This fruit has a yellow skin and contains potassium."
-    },
+market_items = [
 
-    {    
-        "name": "Apple",
-        "price": "1.25"
-        "type of item": "Fruit"
-        "description": "This fruit is round and has a red skin. The fruit is moderately hard."
-    },
-
-    {
-        "name": "Orange",
-        "price": "1.10"
-        "type of item": "Fruit"
-        "description": "This fruit is is round and has an orange skin. The fruit contains citrus."
-
-    }
-
-
-] """
-
-item = {
+{
     "name": "Apple",
-    "price": 0.89,
-    "description": 
+    "price": "1.97/lb",
+    "description": "This fruit is quite round. Its color is red. Apples are high in fiber and vitamin C, making it a healthy fruit option." 
+},
+
+{
+    "name": "Banana",
+    "price": "0.99/lb",
+    "description": "This fruit has a quite of a curved shape. Its color is usually yellow, but it can also sometimes be a bright green. Bananas are high in potassium, making it a healthy fruit option."
+},
+
+{
+    "name": "Orange",
+    "price": "1.79/lb",
+    "description": "This fruit is quite round. Its color is orange. Oranges are high in vitamin C, making it a healthy fruit option."
+},
+
+{
+    "name": "Kiwi",
+    "price": "1.89/lb",
+    "description": "Kiwis have an oval shape. Kiwis have a brown skin on the outside but a green color on the inside. Kiwis are high in vitamin C and can benefit human skin and the digestive system, making it a healthy fruit option."
+},
+
+{
+    "name": "Lemon",
+    "price": "1.89/lb",
+    "description": "Lemons have an oval shape. Its color is yellow. Lemons are high in vitamin C, benefits human skin, the digestive system, and the immune system, making it a healthy fruit option."
+},
+
+{
+    "name": "Lime",
+    "price": "1.29/lb",
+    "description": "Limes have an oval shape. Its color is green. Limes are high in vitamin C. Limes also benefit the immune system and digestive system, which makes it a healthy fruit option."
+},
+
+{
+    "name": "Strawberry",
+    "price": "3.29/lb",
+    "description": "Strawberries have a somewhat triangular shape. Its color is red. Strawberries are high in vitamin C and fiber."
+},
+
+{
+    "name": "Blueberry",
+    "price": "2.89/lb",
+    "description": "Blueberries are round. Its color is a darkish blue. Blueberries are high in vitamin C, vitamin K, and manganese."
 }
+
+]
+
+print("Welcome to our market, where we sell multiple healthy fruits!")
+for index, item in enumerate(market_items):
+    print(index, ":", item["name"])
+
+while True:
+    Choices = int(input("According to the index, what fruits(s) would you like to add to your cart?: "))
+print(f"You have chose to purchase: {market_items[Choices]["name"]}")
