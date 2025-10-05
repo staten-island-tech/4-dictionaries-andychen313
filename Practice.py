9c772d37a8dedcb2aca447acda090e71a22dc6ef
andychen313 <andyc313@nycstudents.net>
2025-10-05 14:04:03 -0400

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -3,5 +3,16 @@
     
 language("Cadee computer never seems to work in the morning.") """
 
+line = input("Give me a piece of writing: ")
+N = int(input("How many lines are there in the piece of writing?: "))
+t_count = 0
+s_count = 0
 
-sentence = input("Give me a sentence: ")
+for _ in range(N):
+    t_count += line.count('t') + line.count('T')
+    s_count += line.count('s') + line.count('S')
+
+if t_count > s_count:
+    print("English")
+else:
+    print("French")
