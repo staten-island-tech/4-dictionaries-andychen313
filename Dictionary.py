504751a680feb16cf2bc7eeb624da41a03ed2996
andychen313 <andyc313@nycstudents.net>
2025-10-05 13:27:31 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -2,49 +2,57 @@ market_items = [
 
 {
     "name": "Apple",
-    "price": "1.97/lb",
+    "price": 1.97,
+    "unit": "/lb",
     "description": "This fruit is quite round. Its color is red. Apples are high in fiber and vitamin C, making it a healthy fruit option." 
 },
 
 {
     "name": "Banana",
-    "price": "0.99/lb",
+    "price": 0.99,
+    "unit": "/lb",
     "description": "This fruit has a quite of a curved shape. Its color is usually yellow, but it can also sometimes be a bright green. Bananas are high in potassium, making it a healthy fruit option."
 },
 
 {
     "name": "Orange",
-    "price": "1.79/lb",
+    "price": 1.79,
+    "unit": "/lb",
     "description": "This fruit is quite round. Its color is orange. Oranges are high in vitamin C, making it a healthy fruit option."
 },
 
 {
     "name": "Kiwi",
-    "price": "1.89/lb",
+    "price": 1.89,
+    "unit": "/lb",
     "description": "Kiwis have an oval shape. Kiwis have a brown skin on the outside but a green color on the inside. Kiwis are high in vitamin C and can benefit human skin and the digestive system, making it a healthy fruit option."
 },
 
 {
     "name": "Lemon",
-    "price": "1.89/lb",
+    "price": 1.89,
+    "unit": "/lb",
     "description": "Lemons have an oval shape. Its color is yellow. Lemons are high in vitamin C, benefits human skin, the digestive system, and the immune system, making it a healthy fruit option."
 },
 
 {
     "name": "Lime",
-    "price": "1.29/lb",
+    "price": 1.29,
+    "unit": "/lb",
     "description": "Limes have an oval shape. Its color is green. Limes are high in vitamin C. Limes also benefit the immune system and digestive system, which makes it a healthy fruit option."
 },
 
 {
     "name": "Strawberry",
-    "price": "3.29/lb",
+    "price": 3.29,
+    "unit": "/lb",
     "description": "Strawberries have a somewhat triangular shape. Its color is red. Strawberries are high in vitamin C and fiber."
 },
 
 {
     "name": "Blueberry",
-    "price": "2.89/lb",
+    "price": 2.89,
+    "unit": "/lb",
     "description": "Blueberries are round. Its color is a darkish blue. Blueberries are high in vitamin C, vitamin K, and manganese."
 }
 
@@ -60,7 +68,7 @@ while True:
         print(index, ":", item["name"])
 
     
-    choices = int(input("According to the index, what fruits(s) would you like to add to your cart?: "))
+    choices = int(input("According to the index or the number that accomodates the fruit, what fruits(s) would you like to add to your cart?: "))
     cart.append(market_items[choices]["name"])
     total += market_items[choices]["price"]
 
@@ -69,4 +77,7 @@ while True:
         break
 
 
-print("Your cart currently contains these following items: )
+print("Your cart currently contains these following items: ")
+for item in cart:
+    print("-", item)
+print(f"Total cost: ${total:.2f}")
