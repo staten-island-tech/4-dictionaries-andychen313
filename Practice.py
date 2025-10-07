752d21667ce2a603afaa6e3c411bd9b1868c01fc
andychen313 <andyc313@nycstudents.net>
2025-10-07 09:26:10 -0400

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -34,8 +34,8 @@ english_or_french()
 parked_spots_today_and_yesterday() """
 
 """ def english_or_french():
-    N = int(input("How many lines are there in the piece of writing?: "))
-    line = input("Give me the lines: ")
+    N = int(input("How many lines are there?: "))
+    line = input("Give me the line(s): ")
     t_count = 0
     s_count = 0
     
@@ -49,7 +49,7 @@ parked_spots_today_and_yesterday() """
         print("French")
 english_or_french() """
 
-def parked_today_and_yesterday():
+""" def parked_today_and_yesterday():
     N = int(input("How many parking spots are there?: "))
     yesterday = input("Use C to indicate an occupied spot and a . to indicate a free spot: ")
     today = input("Use C to indicate an occupied spot and a . to indicate a free spot: ")
@@ -59,4 +59,69 @@ def parked_today_and_yesterday():
         if yesterday[i] == 'C' and today[i] == 'C':
             parked_today_and_yesterday += 1
     print(parked_today_and_yesterday)
-parked_today_and_yesterday()
+parked_today_and_yesterday() """
+
+""" def lang(x):
+    t = 0
+    s = 0
+    for char in x:
+        if char == "t" or char == "T":
+            t+=1
+        elif char == "s" or char == "S":
+            s+=1
+    if t>s:
+        print("Eng")
+    else:
+        print("French") """
+
+""" def spaces(y, t):
+
+spaces("CCC..", ".C.C.C") """
+
+""" def lang():
+    t_count = 0
+    s_count = 0
+    x = input("Give me the line(s): ")
+    for char in x:
+        if char == "t" or char == "T":
+            t+=1
+        elif char == "s" or char == "S":
+            s+=1
+
+    if t>s:
+        print("English")
+    else:
+        print("French")
+lang() """
+
+""" def english_or_french():
+    
+    x = input("Give me the line(s): ")
+    t_count = 0
+    s_count = 0
+    
+    for _ in range():
+        t_count += x.count('t') + x.count('T')
+        s_count += x.count('s') + x.count('S')
+    
+    if t_count > s_count:
+        print("English")
+    else:
+        print("French")
+english_or_french()  """
+
+""" def lang(x):
+    input = ("Give me the line(s): ")
+    t_count = 0
+    s_count = 0
+    for char in x:
+        if char == "t" or char == "T":
+            t+=1
+        elif char == "s" or char == "S":
+            s+=1
+
+    if t>s:
+        print("English")
+    else:
+        print("French")
+lang(input) """
